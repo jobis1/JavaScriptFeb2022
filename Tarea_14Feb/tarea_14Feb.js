c2e42462a27be26494ecd2b4883959676cf125b7
jobis1 <jobis1@gmail.com>
2022-02-14 20:09:47 -0600

    tarea_14Feb "Adding homework"

---
+++ Tarea_14Feb/tarea_14Feb.js
@@ -0,0 +1,5 @@
+//Tarea hacer una funcion de flecha para que en una sola linea regresa el cuadrado de un numero
+
+let cuadrado = (x)=> {return x * x};
+console.log("El Cuadrado es:", cuadrado(8));
+

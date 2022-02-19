1cd6d75f93fa08ff532e3d0b21bcf3cdcf525c3b
jobis1 <jobis1@gmail.com>
2022-02-18 22:29:09 -0600

    tarea_16Feb

--- jest_test/suma.test.js
+++ jest_test/suma.test.js
@@ -1,7 +1,7 @@
 const suma = require ('./suma');
 
 test("Comparar que la suma de 3 y 2 es igual a 5",()=>{
-    expect(suma(3,5)).toBe(5);
+    expect(suma(3,5)).not.toBe(5);
 });
 
 test("Comparar que la suma de 3 y 2 es igual a 5",()=>{

00af4a0aa5b3ca1a5a26fc82650b77e23f4a8573
jobis1 <jobis1@gmail.com>
2022-02-23 20:11:55 -0600

    Merge branch 'main' of https://github.com/jobis1/JavaScriptFeb2022 into tarea_14Feb

--- testCafe/pageObjectModel.js
+++ testCafe/pageObjectModel.js
@@ -1,9 +1,27 @@
+
 import { Selector } from "testcafe";
 
-class Page{
-    constructor(){
 
+const label = Selector('label');
+class feature {
+    constructor (text){
+        this.label = label.withText(text);
+        this.checkbox = this.label.find('input[type=checkbox');
+        this.radio_windows = Selector('input[type=radio][value="Windows"]');
     }
+}
+class Page {
+  constructor() {
+    this.nameInput = Selector("#developer-name");
+    this.checkbox1 = Selector('#remote-testing');
+    this.featureList =[
+        new feature('Support for testing on remote devices'),
+        new feature('Re-using existing JavaScript code for testing'),
+        new feature('Running tests in background and/or in parallel in multiple browsers'),
+        new feature('Easy embedding into a Continuous integration system'),
+        new feature('Advanced traffic and markup analysis')
+    ]
+  }
 }
 
 export default new Page();
--- testCafe/pruebasUI.js
+++ testCafe/pruebasUI.js
@@ -1,7 +1,110 @@
-import pagina from './pageObjectModel';
+import { feature } from "caniuse-lite";
+import pagina from "./pageObjectModel";
 
-fixture('Pruebas de UI usando testCafe para practicar de Nodejs').page('https://devexpress.github.io/testcafe/example/');
+fixture("Pruebas de UI usando testCafe para practicar de Nodejs").page(
+  "https://devexpress.github.io/testcafe/example/"
+);
 
-test('Prueba1: abrir una pagina de pruebas', async t =>{
-    await t ;
+test("Prueba1: abrir una pagina de pruebas", async (t) => {
+  await t.wait(1000);
 });
+
+test("Pruebas2: Escribir un texto", async (t) => {
+  await t
+    .typeText(pagina.nameInput, "Tester")
+    //Asertions
+    .expect(pagina.nameInput.value)
+    .eql("Tester");
+});
+
+test("Pruebas3: Escribir numeros", async (t) => {
+  await t
+    .typeText(pagina.nameInput, "78521")
+    //Asertions
+    .expect(pagina.nameInput.value)
+    .eql("78521")
+    .expect(pagina.nameInput.value)
+    .notEql("Tester");
+});
+
+test("Pruebas4: Escribir signos", async (t) => {
+  await t
+    .typeText(pagina.nameInput, "%^%&%$")
+    //Asertions
+    .expect(pagina.nameInput.value)
+    .eql("%^%&%$")
+    .expect(pagina.nameInput.value)
+    .notEql("Tester");
+});
+
+test("Pruebas5: Usar una variable y un if de pruebas", async (t) => {
+  let variable = 78676;
+  await t.click(pagina.nameInput);
+  if (typeof varible == "number") {
+    await t.expect(pagina.nameInput.value).eql("");
+  } else {
+    await t
+      .typeText(pagina.nameInput, "76876")
+      .expect(pagina.nameInput.value)
+      .eql("76876");
+  }
+});
+
+test("Pruebas6: Escribir un texto y borrar y reemplazar una sola letra", async (t) => {
+  await t
+    .expect(pagina.nameInput.value).eql("")
+    .typeText(pagina.nameInput, "QA")
+    .typeText(pagina.nameInput, "Teter", { replace: true })
+    .typeText(pag.nameInput, "s", { caretPos: 2 })
+
+    //Assertion
+
+    .expect(pagina.nameInput.value)
+    .notEql("Texter")
+    .expect(pagina.nameInput.value)
+    .notEql("QA")
+    .expect(pagina.nameInput.value)
+    .contains("Test")
+    .expect(pagina.nameInput.value)
+    .eql("Tester");
+});
+
+
+test("Pruebas7: Escribir un texto y borrar y reemplazar una sola letra", async t => {
+    await t
+      .expect(pagina.nameInput.value).eql("")
+      .typeText(pagina.nameInput, "Texter")
+      .pressKey('ctrl+a delete', { replace: true })
+      .typeText(pagina.nameInput, 'Tester')
+  
+      //Assertion
+  
+      .expect(pagina.nameInput.value).notEql("Texter")
+      .expect(pagina.nameInput.value).notEql("QA")
+      .expect(pagina.nameInput.value).contains("Test")
+      .expect(pagina.nameInput.value).eql("Tester");
+  });
+
+  test("Pruebas8: clic check box", async t => {
+    await t
+    .expect(pagina.checkbox1.checked).notOk()
+    .click(pagina.checkbox1)
+    .expect(pagina.checkbox1.checked).ok()
+  });
+
+
+  test("Pruebas9: clic check box", async t => {
+    for( const feature of pagina.featureList){
+        await t
+   .click(feature.label)
+   .wait(500)
+   .expect(feature.checkbox.checked).ok()
+    }
+  });
+
+  test("Pruebas10: clic check box", async t => {
+        await t
+   .expect(pagina.radio_windows).notOk()
+   .click(feature.radio_windows)
+   .expect(pagina.radio_windows.checked).ok()
+  });
